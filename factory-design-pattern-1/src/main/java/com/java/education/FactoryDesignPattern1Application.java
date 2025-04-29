522aa9866c0bc19070dd56a49c4b092522ca0892
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-04-29 16:25:50 +0530

    [refactor]
    Code Quality:
    - formatted -   [ ]
    - mvn clean -   [ ]
    - mvn install - [ ]
    
    Related ticket(s):

---
+++ factory-design-pattern-1/src/main/java/com/java/education/FactoryDesignPattern1Application.java
@@ -0,0 +1,13 @@
+package com.java.education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryDesignPattern1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoryDesignPattern1Application.class, args);
+	}
+
+}
---
+++ factory-design-pattern-1/src/test/java/com/java/education/FactoryDesignPattern1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.education;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryDesignPattern1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
