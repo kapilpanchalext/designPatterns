bbae1f291105e26d5efc07b357583c854cd13186
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-05-06 08:30:57 +0530

    [refactor]
    Code Quality:
    - formatted -   [ ]
    - mvn clean -   [ ]
    - mvn install - [ ]
    
    Related ticket(s):

--- adaptorDesignPattern2/src/main/java/com/java/adaptor/sdk/StripeApi.java
+++ adaptorDesignPattern2/src/main/java/com/java/adaptor/sdk/StripeApi.java
@@ -3,6 +3,7 @@ package com.java.adaptor.sdk;
 public class StripeApi {
 
 	public void makePayment(String customerId, double amountInDollars, String currencyCode) {
+		// This is a mock implementation of the Stripe payment gateway
 		System.out.println("Stripe Payment Gateway Received: $" + amountInDollars + " " + currencyCode + " for customerId: " + customerId);
 	}
 }
