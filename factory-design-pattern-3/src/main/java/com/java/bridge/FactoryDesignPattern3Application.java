4811dcbc65a9549dda1552df0df6722907d7008d
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-05-07 11:17:14 +0530

    [bridge] design pattern
    Code Quality:
    - formatted -   [ ]
    - mvn clean -   [ ]
    - mvn install - [ ]
    
    Related ticket(s):

---
+++ factory-design-pattern-3/src/main/java/com/java/bridge/FactoryDesignPattern3Application.java
@@ -0,0 +1,13 @@
+package com.java.bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryDesignPattern3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoryDesignPattern3Application.class, args);
+	}
+
+}
---
+++ factory-design-pattern-3/src/test/java/com/java/bridge/FactoryDesignPattern3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryDesignPattern3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
