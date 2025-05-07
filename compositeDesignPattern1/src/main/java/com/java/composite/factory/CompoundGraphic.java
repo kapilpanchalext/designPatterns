0dc2cd69dc957544c5c8efe81456dd6d0021ea83
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-05-07 12:27:04 +0530

    [composite] design pattern
    Code Quality:
    - formatted -   [ ]
    - mvn clean -   [ ]
    - mvn install - [ ]
    
    Related ticket(s):

---
+++ compositeDesignPattern1/src/main/java/com/java/composite/factory/CompoundGraphic.java
@@ -0,0 +1,31 @@
+package com.java.composite.factory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompoundGraphic implements Graphic{
+	private List<Graphic> children = new ArrayList<>();
+	
+	public void add(Graphic graphic) {
+		children.add(graphic);
+	}
+	
+	public void remove(Graphic graphic) {
+		children.remove(graphic);
+	}
+
+	@Override
+	public void move(int x, int y) {
+		for(Graphic child : children) {
+			child.move(x, y);
+		}
+	}
+
+	@Override
+	public void draw() {
+		for(Graphic child : children) {
+			child.draw();
+		}
+	}
+
+}
