fe2ab80428ff92911cc97e50d989912c9314a460
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-05-08 09:52:26 +0530

    [email] microservice
    Code Quality:
    - formatted -   [ ]
    - mvn clean -   [ ]
    - mvn install - [ ]
    
    Related ticket(s):

---
+++ spring-email-microservice/src/main/java/com/java/email/SpringEmailMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.java.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmailMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmailMicroserviceApplication.class, args);
+	}
+
+}
---
+++ spring-email-microservice/src/test/java/com/java/email/SpringEmailMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmailMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
